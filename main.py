54ec77af826da522c7a5b89477e560dac002baa2
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2024-12-24 08:52:46 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
-from fastapi import HTTPException
+from fastapi import HTTPException,status
+
 
 
 app = FastAPI()
@@ -29,7 +30,7 @@ def get_posts():
     return {"data":my_posts}
 
 #Creating A post and saving it to memory by assigning Random id and Validating from Post Class
-@app.post("/posts")
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_post(post : Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(0,1000000000000000)
@@ -43,7 +44,20 @@ def findpost(id:int):
             return {"post_details": post}
 
     # Raise a 404 error if no post is found with the given id
-    raise HTTPException(status_code=404, detail=f"Post with id {id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
+
+def find_id_index(id:int):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with {id} not found")
+
+
+#always write same api which having parameter and simple get request always write that api first which dont have a parameter passing in it 
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts) - 1]
+    return {"detail":post}
 
 
 #Getting the singal post
@@ -52,7 +66,22 @@ def get_post(id:int):
     post = findpost(id)
 
     return {"postdetail":post}
-    
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_id_index(id)
+
+    my_posts.pop(index)
+    return {'message':f"post having index {2} is deleted"}
+
+@app.put("/posts/{id}")
+def update_post(id:int,post:Post,status_code = status.HTTP_204_NO_CONTENT): #204 is that when no need to return data just update
+    index = find_id_index(id)
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+
+    return {"detail":f"Post with {id} is updated"}
 
 
 
