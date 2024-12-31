4e0654032f6c7135c63fd3980e9421cf98aa610c
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2024-12-31 08:54:34 +0500

    Add files via upload

--- ORM.py
+++ ORM.py
@@ -8,3 +8,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL) # establishing connection db
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind = engine) # this session is used for talking to database and the values inside it are default 
 
 Base = declarative_base() #use for making table in db 
+
+#We using it to contact with db tables and every time we send api request to db or giv sql statment it made session and execute, after close the db
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
