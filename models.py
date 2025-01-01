d2a1f85450579dc2aef32fcb0b56945d33573fc4
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-01-01 08:39:09 +0500

    Use some Best Practices for defining schema

--- models.py
+++ models.py
@@ -1,12 +1,17 @@
 #This file is used for making datamodels or table 
 from sqlalchemy import Column,Integer,String,Boolean
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .ORM import Base
 
 class Post(Base):
-    __table__ = "posts"
+    #you cannot modify tables when created using sqlalchemy if you need to add some thing do it before the table is created, the sql alchemy sees if table is there it wont go further to see changes in it 
+    __tablename__ = "posts"
 
     id = Column(Integer,primary_key=True,nullable=False)
     title = Column(String,nullable=False)
     content = Column(String,nullable=False)
-    published =Column(Boolean,default=True)
+    published =Column(Boolean,server_default='TRUE',nullable=False)
     rating = Column(Integer,nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False,server_default=text('now()')) 
