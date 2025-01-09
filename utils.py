609df687031c321ef81819ee9235a6a0220f4d61
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-01-08 20:29:41 -0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -3,3 +3,7 @@ pwd_context = CryptContext(schemes=["bcrypt"],deprecated = "auto")
 
 def hashpass(password:str):
     return pwd_context.hash(password)
+
+#checking for password correction 
+def verify(plain_password,hashed_password):
+    return pwd_context.verify(plain_password,hashed_password)
