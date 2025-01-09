cff67809f9fcfa3f946182b23116a3d8d9a4fc89
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-01-08 20:30:45 -0800

    Add files via upload

--- mainFileRouters.py
+++ mainFileRouters.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import post, userRouters  # Import routers
+from app.routers import post, userRouters,auth  # Import routers
 from app.ORM import engine
 from app import models
 
@@ -9,6 +9,7 @@ app = FastAPI()
 # Include routers
 app.include_router(post.router)
 app.include_router(userRouters.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def read_root():
