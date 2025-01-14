61008b61ca9e8a3785d28b9e48ef7df3097567fb
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-01-13 20:21:08 -0800

    Add files via upload

--- routers/post.py
+++ routers/post.py
@@ -4,10 +4,10 @@ from typing import List
 from ..ORM import engine,get_db
 from sqlalchemy.orm import Session
 from ..schema import UpdatingPost,CreatePost,ApiResponsetoUser
-
+from app import Oauth2
 
 router = APIRouter(
-    prefix="/post",
+    prefix="/post", # Use as a prefix like (/post/DBposts)
     tags=["Posts"]  # Using For grouping same API on swagger UI
     
 )#Now Working both usecase with routers so we only initialize fastapi once and use it in entire project 
@@ -15,12 +15,12 @@ router = APIRouter(
 
 #Use to retrieve All Post present in Memory
 @router.get("/DBposts",status_code=status.HTTP_200_OK,response_model=List[ApiResponsetoUser])
-def get_Db_Post(db: Session = Depends(get_db)):
+def get_Db_Post(db: Session = Depends(get_db),user_credentials:dict  = Depends(Oauth2.get_current_user)):
     posts = db.query(PostModelDB).all()
     return posts
 
 @router.post("/creatingPost",status_code=status.HTTP_201_CREATED,response_model=ApiResponsetoUser)
-def PostCreated(post:CreatePost,db:Session = Depends(get_db)):
+def PostCreated(post:CreatePost,db:Session = Depends(get_db),user_credentials:dict  = Depends(Oauth2.get_current_user)):
     new_post = PostModelDB(**post.dict())
     db.add(new_post)
     db.commit()
@@ -28,7 +28,7 @@ def PostCreated(post:CreatePost,db:Session = Depends(get_db)):
     return new_post
 
 @router.get("/DBposts/{id}",status_code=status.HTTP_200_OK,response_model=ApiResponsetoUser)
-def getSinglePost(id: int,db:Session = Depends(get_db)):
+def getSinglePost(id: int,db:Session = Depends(get_db),user_credentials:dict  = Depends(Oauth2.get_current_user)):
     post = db.query(PostModelDB).filter(PostModelDB.id == id).first()
     if not post:
         raise HTTPException(
@@ -37,7 +37,7 @@ def getSinglePost(id: int,db:Session = Depends(get_db)):
     return post
 
 @router.delete("/DBposts/{id}",status_code=status.HTTP_200_OK,response_model=ApiResponsetoUser)
-def deletingSimplePostFromDB(id: int,db:Session = Depends(get_db)):
+def deletingSimplePostFromDB(id: int,db:Session = Depends(get_db),user_credentials:dict  = Depends(Oauth2.get_current_user)):
     deleted_post = db.query(PostModelDB).filter(PostModelDB.id == id).first()
     if not deleted_post:
         raise HTTPException(
@@ -47,10 +47,13 @@ def deletingSimplePostFromDB(id: int,db:Session = Depends(get_db)):
     db.delete(deleted_post)
     db.commit()
     return {
-        "message":f"Post With {id} deleted SuccesFully"
+        "title": "Post Deleted",
+        "content": f"Post With ID {id} was successfully deleted.",
+        "published": True,
+        "rating": None,  # Optional field; you can also omit this field
     }
 @router.put("/UpdateDBposts/{id}", status_code=status.HTTP_200_OK,response_model=ApiResponsetoUser)
-def update_post(id: int, post: UpdatingPost,db:Session = Depends(get_db)):
+def update_post(id: int, post: UpdatingPost,db:Session = Depends(get_db),user_credentials:dict  = Depends(Oauth2.get_current_user)):
     # Check if the post exists in the database
     existing_post = db.query(PostModelDB).filter(PostModelDB.id == id).first()
     if not existing_post:
