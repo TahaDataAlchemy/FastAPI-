93eacf96c814eafebd0698694d317ba2e4bc137c
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-01-17 04:19:10 -0800

    Add files via upload

--- routers/post.py
+++ routers/post.py
@@ -12,17 +12,18 @@ router = APIRouter(
     
 )#Now Working both usecase with routers so we only initialize fastapi once and use it in entire project 
 
-
+#Note User_data in the funxtion Prameter contain id and email from login Table
 #Use to retrieve All Post present in Memory
 @router.get("/DBposts",status_code=status.HTTP_200_OK,response_model=List[ApiResponsetoUser])
 def get_Db_Post(db: Session = Depends(get_db),user_data:dict  = Depends(Oauth2.get_current_user)):
-    posts = db.query(PostModelDB).all()
+    # posts = db.query(PostModelDB).all()
+    posts = db.query(PostModelDB).filter(PostModelDB.ownner_id == user_data["id"]).all() #PostModelDb is taking ownner id from db(Post_Table) and User_data is taking id from users table and after matching them it send only those post which matched both ids
     return posts
 
 @router.post("/creatingPost",status_code=status.HTTP_201_CREATED,response_model=ApiResponsetoUser)
 def PostCreated(post:CreatePost,db:Session = Depends(get_db),user_data:dict  = Depends(Oauth2.get_current_user)):
     print(user_data)
-    new_post = PostModelDB(**post.dict())
+    new_post = PostModelDB(ownner_id =user_data['id'],**post.dict()) # Assigining Ownner_id (FK) in post table from with user_id
     db.add(new_post)
     db.commit()
     db.refresh(new_post) #Refresh to get generated Fields
@@ -35,6 +36,10 @@ def getSinglePost(id: int,db:Session = Depends(get_db),user_data:dict  = Depends
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with ID {id} is not Found"
         )
+    if post.id!=user_data["id"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,detail = "Not Allowed to perfrom this operation"
+        )
     return post
 
 @router.delete("/DBposts/{id}",status_code=status.HTTP_200_OK,response_model=ApiResponsetoUser)
@@ -44,6 +49,10 @@ def deletingSimplePostFromDB(id: int,db:Session = Depends(get_db),user_data:dict
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post With ID {id} does not Exist"
+        )
+    if deleted_post.ownner_id!=user_data["id"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,detail = "Not Authorized to perfrom this requested action"
         )
     db.delete(deleted_post)
     db.commit()
@@ -61,6 +70,10 @@ def update_post(id: int, post: UpdatingPost,db:Session = Depends(get_db),user_da
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with ID {id} does not exist"
+        )
+    if existing_post.ownner_id!=user_data["id"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,detail = "Not Authorized to perfrom this requested action"
         )
 
     for key, value in post.dict().items():
