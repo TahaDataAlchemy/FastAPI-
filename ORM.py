457b3526bff4849c704f60a187da8c0e046a7143
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-01-20 19:17:12 -0800

    Add files via upload

--- ORM.py
+++ ORM.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-
-
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost/fastapi'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL) # establishing connection db
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind = engine) # this session is used for talking to database and the values inside it are default 
