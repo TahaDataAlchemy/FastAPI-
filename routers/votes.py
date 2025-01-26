1304d11919fba83ca4aebbefdd0a71ad6057f5f5
Taha Mehboob <157484236+TahaDataAlchemy@users.noreply.github.com>
2025-01-26 06:45:53 -0800

    Add files via upload

--- routers/votes.py
+++ routers/votes.py
@@ -8,8 +8,12 @@ router  = APIRouter(
 
 @router.post("/",status_code=status.HTTP_201_CREATED)
 def vote(vote:schema.Vote,db:Session = Depends(ORM.get_db),current_user:int = Depends(Oauth2.get_current_user)):
+
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with {vote.post_id} does not Exist")
     
-    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user["id"])
+    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user["id"]) #Composite Key (Vote.post_id+current_user_id)
     found_vote = vote_query.first()
     if(vote.dir == 1):
         if found_vote:
